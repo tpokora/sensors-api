3d3161eb792c6b29380de96692579dfabbb13cac
tpokora <pokora.tomek@gmail.com>
2019-04-21 12:05:53 +0200

    Reading Quickstart sheets

--- src/main/java/org/tpokora/sheets/SheetsController.java
+++ src/main/java/org/tpokora/sheets/SheetsController.java
@@ -1,22 +1,23 @@
 package org.tpokora.sheets;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.tpokora.sheets.SheetsService;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.List;
 
-//@RestController
+@RestController
 public class SheetsController {
 
 //    @Autowired
     SheetsService sheetsService;
 
-//    @RequestMapping("/sheets")
-    public String sheets() throws GeneralSecurityException, IOException {
-        sheetsService.test();
-        return "Sheets";
+    @RequestMapping("/sheets")
+    public ResponseEntity<List<List<Object>>> sheets() throws GeneralSecurityException, IOException {
+        ReadSheets readSheets = new ReadSheets();
+        return new ResponseEntity<>(readSheets.test(), HttpStatus.OK);
     }
 }
